18d6aee5d908b70b427c64d24f838b572bbcacbc
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-03-31 21:01:58 +0530

    Time: 27 ms (96.62%) | Memory: 64.3 MB (76.79%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
+        // 2n similar type solution
         int n = weights.size();
-        vector<int> result;
-        for(int i = 0; i<n-1; i++)
+        int m = n-1;
+        vector<int>result(m,0);
+        for(int i=0;i<m;i++)
         {
-            
-            
-                int sum = weights[i] + weights[i+1];
-                result.push_back(sum);
-            
+            result[i] = weights[i] + weights[i+1];
         }
-        sort(begin(result),end(result));
-        int s = result.size();
-        long mini = 0;
+        sort(begin(result), end(result));
+        long mini = 0; 
         long maxi = 0;
-        for(int  i=0; i<k-1; i++)
+        for(int i=0; i<k-1;i++)
         {
-            mini+= result[i];
-            maxi += result[s-1-i];
+            mini +=result[i];
+            maxi += result[m-1-i];
         }
         return maxi-mini;
     }
